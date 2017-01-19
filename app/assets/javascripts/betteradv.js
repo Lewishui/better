b497296c09b8d81e1117798729a273fe8baf601e
lewishui <lewis.hui@qq.com>
2017-01-19 17:59:41 +0800

    011918
    up

--- app/assets/javascripts/betteradv.js
+++ app/assets/javascripts/betteradv.js
@@ -42,6 +42,269 @@
 // {
 //     return val/10000;
 // }
+///////////////////////////////////////
+$(function () {
+  var chart_symbols = [];
+  var symbols = [];
+  $(".forex-chart[data-symbol]").each(function(){
+    chart_symbols.push( $(this).data('symbol') );
+    symbols.push( $(this).data('symbol') );
+  });
+  $(".b-instrument-last-quote[data-symbol]").each(function(){
+    var $this = $(this);
+    var symbol = $this.data('symbol');
+    //there maybe more than one label
+    if( !g_quotation_desc.labels[symbol] )
+    {
+      g_quotation_desc.labels[symbol] = $(".b-instrument-last-quote[data-symbol='"+symbol+"']");
+    }
+    if( symbols.indexOf( symbol) == -1)
+    {
+      symbols.push( symbol );
+    }
+  });
+    Highcharts.setOptions({
+        global : {
+            useUTC : false
+        }
+    });
+    if( symbols.length >0 )
+    {
+      // var source = new EventSource('http://www.ballmerasia.com/node/sse/'+symbols.join(','));
+      var source = new EventSource('http://www.ballmerasia.com:8080/sse/'+symbols.join(','));
+      source.addEventListener('message', function(e) {
+        var data = JSON.parse(e.data);
+        if( g_quotation_desc.first_pass )
+        {
+          g_quotation_desc.first_pass = false;
+          InitializeChart( data );
+        }else{
+          for( var i = 0; i< symbols.length; i++)
+          {
+            var symbol = symbols[i];
+            var time_price = data[symbol];
+            var time = (new Date( parseInt(time_price) )).getTime();
+            var price= ConvertIntegerToCorrectRate( symbol, parseInt(time_price.split('_')[1]));
+            var formatted_price = format_forex_price( price );
+            //console.log("data=%s,%s", time, price);
+            if(g_quotation_desc.panels[symbol])
+            {
+               g_quotation_desc.panels[symbol].update( time, price, 1, 0 );
+            }
+            if(g_quotation_desc.labels[symbol])
+            {
+              g_quotation_desc.labels[symbol].html(formatted_price );
+            }
+            // new point added
+            //g_quotation_desc.charts[symbols[i]].yAxis[0].plotLines[0].value = price;
+          }
+        }
+        console.log(e);
+      }, false);
+    }
+});
+
+function InitializeChart(message){
+
+  $(".forex-chart").each(function(){
+    var $container = $(this);
+    var symbol = $container.data('symbol');
+    var panel = new BetterFinancialPanel( symbol );
+    panel.drawCharts( message[symbol] );
+
+    //FinancialPanel.drawChart( this.id, symbol, message[symbol]);
+
+    g_quotation_desc.charts[symbol] = panel.lineChart;
+    g_quotation_desc.panels[symbol] = panel;
+  })
+  // Create the chart
+}
+function BetterFinancialPanel( symbol )
+{
+  this.urlBaseSecure = 'http://www.ballmerasia.com:8080/';
+
+  this.lineChart = null;
+  this.instrumentID = symbol;
+  this.lastQuotes= [];
+  this.lastTradeID = 0;
+
+  this.currentMinute = Math.floor(this.time / 60000) * 60000;
+  Highcharts.setOptions({
+      global: {
+          useUTC: false
+      }
+  });
+}
+BetterFinancialPanel.prototype.renderCharts =function(){
+  var data = this.getHistory();
+  this.drawCharts( data );
+}
+BetterFinancialPanel.prototype.getHistory = function(){
+  // start, from, symbol
+  var url =  this.urlBaseSecure+ "/forex_history/"+this.instrumentID;
+  return $.ajax({url:url}).then( function(res) {
+    return this.fixData(res);})
+}
+BetterFinancialPanel.prototype.fixData = function( rawData){
+  rawDtata = rawData.sort();
+  var data = [];
+  for (var i = 0; i < rawDtata.length; i += 1) {
+    data.push([
+      (new Date( parseInt(rawDtata[i]) )).getTime(),
+      this.convertIntegerToCorrectRate( parseInt(rawDtata[i].split('_')[1]))
+    ]);
+  }
+  return data;
+}
+
+BetterFinancialPanel.prototype.convertIntegerToCorrectRate = function ( symbol, val  )
+{
+  if( symbol == "USUSDJPY")
+  {
+    return val/100;
+  }else{
+    return val/10000;
+  }
+}
+BetterFinancialPanel.prototype.drawCharts = function(chartData, b) {
+      var c = this.instrumentID;
+      var f = this.currentMinute;
+      var g = new Date();
+      var e = [];
+      e = charData;
+
+      var seriesType = 'line';
+      var lineColor = Registry.chartConfig.financialPanel.colors.line;
+      var fillColor = {
+                          linearGradient : {
+                              x1: 0,
+                              y1: 0,
+                              x2: 0,
+                              y2: 1
+                          },
+                          stops : [[0, Registry.chartConfig.financialPanel.colors.fillColor.top],
+                                   [1, Registry.chartConfig.financialPanel.colors.fillColor.bottom]]
+                      };
+
+      var a = new Highcharts.StockChart({
+          xAxis: {
+              gridLineWidth: 1,
+              gridLineColor: Registry.chartConfig.financialPanel.colors.axisgrid,
+              lineColor: Registry.chartConfig.financialPanel.colors.axis,
+              tickLength: 0,
+              ordinal: false,
+              labels: {
+                  formatter: function() {
+                      g.setTime(this.value);
+                      var j = "H:i";
+                      var h = this.axis.series[0].data;
+                      if (h.length && (h[h.length - 1].x - h[0].x < (5 * 60000))) {
+                          j = "H:i:s"
+                      }
+                      return Ext.Date.format(g, j)
+                  }
+              }
+          },
+          yAxis: {
+              id: "advanced-chart-line-y-axis-" + c,
+              gridLineColor: Registry.chartConfig.financialPanel.colors.axisgrid,
+          },
+          chart: {
+              renderTo: "advanced-chart-line-" + c,
+              plotBorderWidth: 1,
+              plotBorderColor: Registry.chartConfig.financialPanel.colors.plotBorder,
+              backgroundColor: 'rgba(255,255,255,0)',
+              plotBackgroundColor: Registry.chartConfig.financialPanel.colors.plotBackgroundColor
+          },
+          rangeSelector: {
+              enabled: false
+          },
+          navigator: {
+              enabled: false
+          },
+          scrollbar: {
+              enabled: false
+          },
+          credits: {
+              enabled: false
+          },
+          series: [{
+              id: "advanced-chart-line-series-" + c,
+              name: "Price",
+              data: e,
+              type: seriesType,
+              threshold : null,
+              fillColor: fillColor
+          }],
+          plotOptions: {
+              line: {
+                  lineWidth: 1,
+                  lineWidth: Registry.chartConfig.financialPanel.lineWidth,
+                  dataGrouping: {
+                      enabled: false
+                  },
+                  marker: {
+                      states: {
+                          hover: {
+                              lineColor: Registry.chartConfig.colors.guide,
+                              radius: 2
+                          }
+                      }
+                  },
+                  events: {
+                      click: function(h) {
+                          Trading.app.getController("Game").selectClosestTradePoint(h.point)
+                      }
+                  },
+                  allowPointSelect: false
+              },
+              series: {
+                color: lineColor,
+                lineWidth: Registry.chartConfig.financialPanel.lineWidth,
+                  states: {
+                      hover: {
+                          lineWidth: 1
+                      }
+                  }
+              }
+          },
+          tooltip: {
+              headerFormat: "<span>{point.key}</span><br/>",
+              xDateFormat: "%H:%M:%S",
+              pointFormat: "<span>{point.y}</span>",
+              borderWidth: 1,
+              crosshairs: [{
+                  color: Registry.chartConfig.colors.guide,
+                  dashStyle: "longdash"
+              }],
+              formatter: function() {
+                  var o = this.points[0].point;
+                  var n = "<span>" + Ext.Date.format(new Date(o.x), "H:i:s") + "</span><br/><span>" + o.y + "</span>";
+                  if (o.marker && o.marker.keep) {
+                      var m = (o.tooltipData.direction == 1) ? Registry._["label-above"] : Registry._["label-below"];
+                      n = '<span class="tooltip-label">' + Registry._["game-label-expiry"] + ":</span><span> " + Ext.Date.format(new Date(o.tooltipData.expiry), "H:i:s") + '</span><br/><span class="tooltip-label">' + m + " " + o.y + '</span><br/><span class="tooltip-label">' + Registry._["trade-info-investment"] + ":</span><span> " + Registry.baseCurrencySymbol + o.tooltipData.stake + '</span><br/><span class="tooltip-label">' + Registry._["trade-info-payout"] + ":</span><span> " + o.tooltipData.payout + '%</span><br/><span class="tooltip-label">' + Registry._["label-rebate"] + ":</span><span> " + o.tooltipData.rebate + "%</span>";
+                      n += Ext.isEmpty(o.tooltipData.returnedAmount) ? "": '<br/><span class="tooltip-label">' + Registry._["label-return-amount"] + ":</span><span> " + Registry.baseCurrencySymbol + o.tooltipData.returnedAmount + "</span>";
+                      if (o.tooltipData.social) {
+                          var q = o.tooltipData.social.userID;
+                          var h = Registry.socialImageUrlPattern.replace("[[[userID]]]", q) + "?v=" + Math.floor(new Date().getTime() / 10000);
+                          var k = o.tooltipData.social.nickname;
+                          var l = (o.tooltipData.direction == 1) ? "images/small-green-arrow-up-10x11.png": "images/small-red-arrow-down-10x11.png";
+                          var m = (o.tooltipData.direction == 1) ? Registry._["short-text-call"] : Registry._["short-text-put"];
+                          var j = Ext.isEmpty(o.tooltipData.returnedAmount) ? Registry._["short-text-opened"] : Registry._["short-text-closed"];
+                          var p = Ext.isEmpty(o.tooltipData.returnedAmount) ? "": '<br/><span class="tooltip-gain">' + Registry._["short-text-gain"] + ": " + Registry.baseCurrencySymbol + o.tooltipData.returnedAmount + "</span>";
+                          n = '<div id="tooltip-social-container"><div class="social-user-img-container"><img class="social-user-img" src="' + h + '" /><img class="social-user-arrow-img" src="' + l + '">&nbsp;</img></div><div class="advanced-social-trade-info"><span class="tooltip-nickname">' + k + ((Registry.env == "development") ? " (" + o.tooltipData.tradeID + ") ": "") + '</span><br/><span class="tooltip-status">' + j + " " + Registry._["short-text-a-binary"] + " " + m + " option</span>" + p + "</div></div>"
+                      }
+                  }
+                  return '<div class="tooltip-container">' + n + "</div>"
+              },
+              useHTML: true
+          }
+      });
+      this.lineChart = a;
+      //this.candlestickChart = this.drawCandlestickChart(c, "advanced-chart-candlestick-", b, e);
+      //this.markTrades(c, Trading.app.getController("User").trades.data.items);
+      //this.markSocialTrades(c)
+}
 
  // A label on the side of chart 2 that points at the last added value
 Highcharts.wrap(Highcharts.Series.prototype, 'addPoint',
